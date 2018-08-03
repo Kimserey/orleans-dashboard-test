d44f3fbff299a83c97aa536fa4096c8a3d31d4dd
Kimserey <kimserey.lam@groomgy.com>
2018-08-03 22:31:47 +0100

    Setup swagger on Client.

--- OrleansDashboardTest.Web/AccountController.cs
+++ OrleansDashboardTest.Web/AccountController.cs
@@ -16,10 +16,18 @@ namespace OrleansDashboardTest.Web
             _factory = factory;
         }
 
+        [HttpPost]
         public async Task<ActionResult<double>> Post(AccountDto dto)
         {
             var grain = _factory.GetGrain<IAccount>(0);
             await grain.SetBalance(dto.Value);
+            return await grain.GetBalance();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<double>> Get()
+        {
+            var grain = _factory.GetGrain<IAccount>(0);
             return await grain.GetBalance();
         }
     }
--- OrleansDashboardTest.Web/Startup.cs
+++ OrleansDashboardTest.Web/Startup.cs
@@ -2,6 +2,13 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Runtime;
+using OrleansDashboardTest.GrainInterfaces;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.Threading.Tasks;
 
 namespace OrleansDashboardTest.Web
 {
@@ -9,9 +16,25 @@ namespace OrleansDashboardTest.Web
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            IClusterClient client = new ClientBuilder()
+                .UseLocalhostClustering()
+                .ConfigureApplicationParts(x => x.AddApplicationPart(typeof(IAccount).Assembly).WithReferences())
+                .ConfigureLogging(x => x.AddConsole())
+                .Build();
+
+            services.AddSingleton<IGrainFactory>(sp => {
+                client.Connect().Wait();
+                return client;
+            });
+
             services
                 .AddMvc()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -20,6 +43,12 @@ namespace OrleansDashboardTest.Web
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseMvc();
         }
