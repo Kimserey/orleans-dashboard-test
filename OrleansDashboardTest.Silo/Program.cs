f134dfd516d23a7c83377d268d9776e443313ec3
Kimserey <kimserey.lam@groomgy.com>
2018-08-03 21:04:19 +0100

    added silo project

---
+++ OrleansDashboardTest.Silo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OrleansDashboardTest.Silo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
